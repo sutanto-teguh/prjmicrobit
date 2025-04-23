b35c9b477f41d7dadcf498cf8beb15c387ef64f3
sutanto-teguh <89557882+sutanto-teguh@users.noreply.github.com>
2025-04-24 05:42:29 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+input.onGesture(Gesture.Shake, function () {
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    jumlah += 1
+    basic.showNumber(jumlah)
+})
 let jumlah = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+jumlah = 0
 basic.forever(function () {
-    if (input.isGesture(Gesture.Shake)) {
-        music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-        jumlah += 1
-        basic.showNumber(jumlah)
-    }
+	
 })
